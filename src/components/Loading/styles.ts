7243cd429cd9207ca406a7800d7ce69391f47098
natan borges <natanmelo5@hotmail.com>
2023-06-23 10:01:35 -0300

    First Commit

---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600}
+`;
+export const LoadIndicator = styled.ActivityIndicator.attrs(({theme}) => ({
+    size: 60,
+    color: theme.COLORS.GREEN_700,
+    animating: true
+}))``
